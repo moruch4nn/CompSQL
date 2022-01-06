a80c506c07731c5d797f6b3e2383e23270192c0c
moru3_48 <moru348@gmail.com>
2022-01-06 13:16:09 +0900

    bug fix.

--- src/main/kotlin/dev/moru3/compsql/SQL.kt
+++ src/main/kotlin/dev/moru3/compsql/SQL.kt
@@ -147,7 +147,7 @@ abstract class SQL(final override var url: String, val properties: Properties): 
 
     override fun <T> get(type: Class<T>): List<T> = get(type) { }
 
-    override fun <T> get(type: Class<T>, where: FirstWhere): List<T> = get(type) { this@get.where = where }
+    override fun <T> get(type: Class<T>, where: FirstWhere): List<T> = get(type) { this.where = where }
 
     override fun <T> get(type: Class<T>, action: Select.() -> Unit): List<T> {
         val columns = mutableMapOf<String, Field>().also { columns ->
